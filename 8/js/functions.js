fd9ebf69b642a999074466488976408f750bfc42
Keks <keks@htmlacademy.ru>
2023-07-25 17:21:04 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/functions.js
@@ -0,0 +1,23 @@
+
+const leadToMinutes = (originalTime) => {
+  const timeInArray = originalTime.split(':');
+  if (typeof originalTime === 'string' && timeInArray.length <= 2) {
+    timeInArray[0] = Number(timeInArray[0]);
+    timeInArray[1] = Number(timeInArray[1]);
+    return Number(timeInArray[0] * 60 + timeInArray[1]);
+  }
+  return null;
+};
+
+const timeChecker = (workStart, workEnd, meetStart, meetTime) => {
+
+  const meetEnd = leadToMinutes(meetStart) + meetTime;
+
+  if (leadToMinutes(workStart) <= leadToMinutes(meetStart) && leadToMinutes(workEnd) >= meetEnd) {
+    return true;
+  }
+  return false;
+};
+
+timeChecker('08:00', '17:30', '14:00', 90); //true
+timeChecker('8:00', '17:30', '08:00', 900); //false
