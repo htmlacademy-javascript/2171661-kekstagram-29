bdd6c1d46a7147649633407e0834a6928417db7e
L1skin <109242490+L1skin@users.noreply.github.com>
2023-07-26 13:10:23 +0300

    загрузил пропущенные в прошлом таске библиотеки, готовлю базу под форму загрузки

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,5 @@
+const ALERT_SHOW_TIME = 3000;
+
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -18,8 +20,30 @@ const getRandomArrayElement = (array) =>
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = '100';
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = '0';
+  alertContainer.style.top = '0';
+  alertContainer.style.right = '0';
+  alertContainer.style.padding = '15px 3px';
+  alertContainer.style.fontSize = '26px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'tomato';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
 
 export {getRandomInteger};
 export {idGenerator};
 export {getRandomArrayElement};
 export {isEscapeKey};
+export {showAlert};
